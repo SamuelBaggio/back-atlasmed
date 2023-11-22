745a3b195be5510b9e81a96b42960badf6314465
samuel <samuelsbaggio@gmail.com>
2023-11-22 10:04:20 -0300

    commit inicial

---
+++ src/main/java/br/com/fiap/atlasmed/AtlasmedApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.atlasmed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlasmedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlasmedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/atlasmed/AtlasmedApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.atlasmed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlasmedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
